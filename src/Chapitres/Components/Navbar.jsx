ab94be1660309976b3734d8ce5fde2ef7f53fbe7
Luna-bf <lburattifayolle@gmail.com>
2025-08-18 15:40:32 +0200

    Changed logo

--- src/Chapitres/Components/Navbar.jsx
+++ src/Chapitres/Components/Navbar.jsx
@@ -1,13 +1,16 @@
 // J'importe mon logo
-import favicon from '../../assets/imgs/favicon.png'
+import logo from '../../assets/imgs/logo.png'
 
 const Navbar = () => {
+    const logoWidth = {width: '3%'};
+    const logoRadius = {borderRadius: '50%'};
+
     return (
         <>
             <header>
                 <nav className="bg-sky-100 text-sky-700 p-3 border-b-2 border-solid border-sky-900">
                     <div className="flex place-content-between items-center">
-                        <a href="#" className="ml-2"><img src={favicon} alt="Logo" /></a>
+                        <a href="#" className="ml-2" style={logoWidth}><img src={logo} alt="Logo" style={logoRadius} /></a>
                         <ul className="flex">
                             <li><a href="#" className="p-5">Accueil</a></li>
                             <li><a href="#" className="p-5">Témoignages</a></li>
