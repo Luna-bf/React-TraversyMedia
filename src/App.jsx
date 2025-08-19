995e0fbc58d0571963b43241a5f32cad112576e3
Luna-bf <lburattifayolle@gmail.com>
2025-08-19 14:25:34 +0200

    Update

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+// Importation de plusieurs éléments depuis React router dom (package installé)
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+// import HomePage from './Chapitres/pages/HomePage';
+
+const router = createBrowserRouter(
+    createRoutesFromElements(<Route index element={<h1>Hi</h1>} />)
+);
+
+const App = () => {
+    return <RouterProvider router={router} />
+
+    // <>
+    {/* J'importe le composant Navbar dans le fichier HomePage */ }
+    {/* <Navbar /> */ }
+
+    {/* 
+                Les props fonctionnent comme les attributs HTML, sauf que je peux aussi passer des données JS, comme des
+                objets ou des fonctions (voir HomePage.jsx)
+            */}
+    {/* Je déclare une prop nommée title prenant une valeur de type string */ }
+    {/* <Hero title="Become a React Dev" subtitle="Find the React job that fits your skill set" /> */ }
+    {/* <Hero /> */ }
+
+    {/* Wrapper components */ }
+    {/* <HomeCards />
+
+            <JobListings />
+            <ViewAllJobs /> */}
+    // </>
+}
+
+export default App
