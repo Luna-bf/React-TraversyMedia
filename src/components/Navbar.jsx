e1cc3255b1675e3524a0ddfa791ff451aeba2a6b
Luna-bf <lburattifayolle@gmail.com>
2025-08-19 15:59:08 +0200

    Implemented Link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
+// Link permet à l'utilisateur de naviguer sur la page sans que celle-ci ne s'actualise
+import { Link } from 'react-router-dom';
 // J'importe mon logo
-import logo from '../assets/imgs/logo.png'
+import logo from '../assets/imgs/logo.png';
 
 const Navbar = () => {
     // const logoWidth = { width: '3%' };
@@ -11,12 +13,13 @@ const Navbar = () => {
                 <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
                     <div className="flex h-20 items-center justify-between">
                         <div className="flex flex-1 items-center justify-center md:items-stretch">
-                            <a href="/index.html" className="flex flex-shrink-0 items-center mr-4"><img src={logo} alt="React jobs logo" className="h-10 w-auto" /><span className="hidden md:block text-white text-2xl font-bold ml-2">React Jobs</span></a>
+                            <Link to="/" className="flex flex-shrink-0 items-center mr-4"><img src={logo} alt="React jobs logo" className="h-10 w-auto" /><span className="hidden md:block text-white text-2xl font-bold ml-2">React Jobs</span></Link>
                             <div className="md:ml-auto">
                                 <div className="flex space-x-2">
-                                    <a href="/index.html" className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">Home</a>
-                                    <a href="/jobs" className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">Jobs</a>
-                                    <a href="/add-job" className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">Add Jobs</a>
+                                    {/* Le slash "/" représente la page d'accueil du site (HomePage) */}
+                                    <Link to="/" className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">Home</Link>
+                                    <Link to="/jobs" className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">Jobs</Link>
+                                    <Link to="/add-job" className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">Add Jobs</Link>
                                 </div>
                             </div>
                         </div>
