c92af935ea67e47e849525abdbb6f0df51cf577f
Luna-bf <lburattifayolle@gmail.com>
2025-08-19 10:50:18 +0200

    Update nav

--- src/Chapitres/Components/Navbar.jsx
+++ src/Chapitres/Components/Navbar.jsx
@@ -2,23 +2,21 @@
 import logo from '../../assets/imgs/logo.png'
 
 const Navbar = () => {
-    const logoWidth = {width: '3%'};
-    const logoRadius = {borderRadius: '50%'};
+    const logoWidth = { width: '3%' };
+    const logoRadius = { borderRadius: '50%' };
 
     return (
         <>
-            <header>
-                <nav className="bg-sky-100 text-sky-700 p-3 border-b-2 border-solid border-sky-900">
-                    <div className="flex place-content-between items-center">
-                        <a href="#" className="ml-2" style={logoWidth}><img src={logo} alt="Logo" style={logoRadius} /></a>
-                        <ul className="flex">
-                            <li><a href="#" className="p-5">Accueil</a></li>
-                            <li><a href="#" className="p-5">Témoignages</a></li>
-                            <li><a href="#" className="p-5">Contact</a></li>
-                        </ul>
-                    </div>
-                </nav>
-            </header>
+            <nav className="bg-indigo-700 border-b border-indigo-500 text-white">
+                <div className="flex place-content-between items-center">
+                    <a href="#" className="ml-2" style={logoWidth}><img src={logo} alt="Logo" style={logoRadius} /></a>
+                    <ul className="flex">
+                        <li><a href="#" className="p-5">Accueil</a></li>
+                        <li><a href="#" className="p-5">Témoignages</a></li>
+                        <li><a href="#" className="p-5">Contact</a></li>
+                    </ul>
+                </div>
+            </nav>
         </>
     )
 }
