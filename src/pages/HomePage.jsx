46051e2001fd908191c836a184709d77cfda136f
Luna-bf <lburattifayolle@gmail.com>
2025-08-19 14:39:50 +0200

    Updated imports

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,16 @@
-import Navbar from '../Chapitres/components/Navbar.jsx'
-import Hero from "../Chapitres/components/Hero.jsx";
-import HomeCards from "../Chapitres/components/HomeCards.jsx";
-import JobListings from "../Chapitres/components/JobListings.jsx";
-import ViewAllJobs from "../Chapitres/components/ViewAllJobs.jsx";
+import Navbar from '../components/Navbar.jsx'
+import Hero from "../components/Hero.jsx";
+import HomeCards from "../components/HomeCards.jsx";
+import JobListings from "../components/JobListings.jsx";
+import ViewAllJobs from "../components/ViewAllJobs.jsx";
 
 const HomePage = () => {
     return (
         <>
+            <Navbar />
             <Hero />
         </>
     )
 }
 
-export default HomePage
+export default HomePage;
