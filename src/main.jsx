1867fecf720e5c6ba4351cba108c55e07a40edd0
Luna-bf <lburattifayolle@gmail.com>
2025-08-19 10:44:58 +0200

    New import

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import JsxCrashCourse from './Chapitres/JsxCrashCourse.jsx'
+// import JsxCrashCourse from './Chapitres/JsxCrashCourse.jsx'
 import HomePage from './Chapitres/HomePage.jsx'
 
 createRoot(document.getElementById('root')).render(
