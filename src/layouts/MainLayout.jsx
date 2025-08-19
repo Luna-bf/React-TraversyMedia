44fdb0eddc2fd931ad332c961159f4eae40265ad
Luna-bf <lburattifayolle@gmail.com>
2025-08-19 15:29:40 +0200

    Added Navbar to MainLayout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -4,7 +4,9 @@ import Navbar from "../components/Navbar";
 const MainLayout = () => {
   return (
     <>
-        {/* Outlet sert de placeholder, il va déterminer quels composants enfants doivent être affichés dans le composant parent,
+        {/* Le composant Navbar sera sur toutes les pages (voir App.jsx pour plus de précisions) */}
+        <Navbar />
+        {/* Outlet sert de remplissage, il va déterminer quels composants enfants doivent être affichés dans le composant parent,
             exemple : Pour l'instant mon composant HomePage n'affiche que le composant Hero car il est le seul a avoir été déclaré
             après avoir été importé, si je déclare le composant HomeCards, Outlet affichera celui-ci dans le navigateur.
         */}
