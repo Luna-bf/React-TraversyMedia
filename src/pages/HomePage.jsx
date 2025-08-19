7ff72503691033161478a09a919d04fe0eda162e
Luna-bf <lburattifayolle@gmail.com>
2025-08-19 14:32:46 +0200

    Moved folders

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import Navbar from '../Chapitres/components/Navbar.jsx'
+import Hero from "../Chapitres/components/Hero.jsx";
+import HomeCards from "../Chapitres/components/HomeCards.jsx";
+import JobListings from "../Chapitres/components/JobListings.jsx";
+import ViewAllJobs from "../Chapitres/components/ViewAllJobs.jsx";
+
+const HomePage = () => {
+    return (
+        <>
+            <Hero />
+        </>
+    )
+}
+
+export default HomePage
