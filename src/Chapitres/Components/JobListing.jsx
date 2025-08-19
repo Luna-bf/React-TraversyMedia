b8104f9b145f800aeb352a712f9e4563e8328038
Luna-bf <lburattifayolle@gmail.com>
2025-08-19 10:43:28 +0200

    Updated jobs

--- src/Chapitres/Components/JobListing.jsx
+++ src/Chapitres/Components/JobListing.jsx
@@ -1,37 +1,36 @@
-const JobListing = () => {
+const JobListing = ({ job }) => { /* Je passe l'élément job en tant que paramètre à JobListing */
     return (
-        <div className='bg-white rounded-xl shadow-md relative'>
-            <div className='p-4'>
-                <div className='mb-6'>
-                    <div className='text-gray-600 my-2'></div>
-                    <h3 className='text-xl font-bold'></h3>
-                </div>
+        <>
 
-                <div className='mb-5'>{description}</div>
+                <div className="bg-white rounded-xl shadow-md relative">
+                    <div className="p-4">
+                        <div className="mb-6">
+                            {/* Je vais chercher les données en déclarant le paramètre job (élément parcouru lors du tour de boucle)
+                                suivi d'un point puis de la clé de l'élément désiré, ici j'affiche la valeur de la clé 'type' */}
+                            <div className="text-gray-600 my-2">{job.type}</div>
+                            <h3 className="text-xl font-bold">{job.title}</h3>
+                        </div>
 
-                <button
-                    className='text-indigo-500 mb-5 hover:text-indigo-600'
-                >
+                        <div className="mb-5">
+                            {job.description}
+                        </div>
 
-                </button>
+                        <h3 className="text-indigo-500 mb-2">{job.salary} / Year</h3>
 
-                <h3 className='text-indigo-500 mb-2'></h3>
+                        <div className="border border-gray-100 mb-5"></div>
 
-                <div className='border border-gray-100 mb-5'></div>
+                        <div className="flex flex-col lg:flex-row justify-between mb-4">
+                            <div className="text-orange-700 mb-3">{job.location}</div>
+                        </div>
 
-                <div className='flex flex-col lg:flex-row justify-between mb-4'>
-                    <div className='text-orange-700 mb-3'>
-                        <a href="#" className='inline text-lg mb-1 mr-1' />
-
+                        {/* J'utilise l'id de l'élément job afin que le lien me renvoie vers une page contenant les infos de la 
+                            carte sur laquelle j'ai cliqué, pour le moment cela ajoute un chiffre dans le lien de la page */}
+                        {/* Je met bien un / avant car je veux que mon lien commence à partir de 'jobs', sinon le lien donnera
+                                "localhost/jobs/jobs/job.id" */}
+                        <a href={`/jobs/${job.id}`} className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm">Read more</a>
                     </div>
-                    <a href=""
-                        className='h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm'
-                    >
-                        Read More
-                    </a>
                 </div>
-            </div>
-        </div>
+        </>
     )
 }
 
--- src/Chapitres/Components/JobListings.jsx
+++ src/Chapitres/Components/JobListings.jsx
@@ -1,13 +1,31 @@
-const JobListing = () => {
+// J'importe le fichier json
+import jobs from '../../jobs.json';
+import JobListing from './JobListing';
+
+const JobListings = () => {
+  // console.log(jobs);
+  // J'utilise la méthode slice() afin de récupérer les 3 premiers éléments de jobs, soit les éléments les plus récents
+  const recentJobs = jobs.slice(0, 3);
+
   return (
-    <section className='bg-blue-50 px-4 py-10'>
-      <div className='container-xl lg:container m-auto'>
-        <h2 className='text-3xl font-bold text-indigo-500 mb-6 text-center'></h2>
-        <div className='grid grid-cols-1 md:grid-cols-3 gap-6'></div>
-        
-      </div>
-    </section>
+    <>
+      <section className='bg-blue-50 px-4 py-10'>
+        <div className='container-xl lg:container m-auto'>
+          <h2 className='text-3xl font-bold text-indigo-500 mb-6 text-center'>Browse jobs</h2>
+          <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+
+            {/* J'utilise la méthode map pour parcourir l'objet jobs, la méthode prend un paramètre 'job' (élément parcouru lors du tour de boucle) */}
+            {recentJobs.map((job) => (
+
+              // Revoir l'utilisation des props
+              // Je passe l'id à JobListing (qui sera le container/carte pour chaque élément de jobs) afin d'éviter d'avoir des erreurs
+              <JobListing key={job.id} job={job} /> /* Puis je donne la prop au composant JobListing */
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
-export default JobListing
+export default JobListings
