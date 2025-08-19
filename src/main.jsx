a5a8d08dc5d09118184fb77da3b88af4aaa8298c
Luna-bf <lburattifayolle@gmail.com>
2025-08-19 14:37:49 +0200

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import App from './App'
 // import JsxCrashCourse from './Chapitres/JsxCrashCourse.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <JsxCrashCourse /> */}
+    <App />
   </StrictMode>,
 )
